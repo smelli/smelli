f91cc341be08a62851fda4a957eadf8ef8492444
Aleks Smolkovic <67457577+alekssmolkovic@users.noreply.github.com>
2026-06-14 19:05:58 +0200

    Replace deprecated pkg_resources import (#71)

--- smelli/__init__.py
+++ smelli/__init__.py
@@ -1,9 +1,9 @@
 from ._version import __version__, __flavio__version__
 import flavio
-from pkg_resources import packaging
+from packaging import version
 _flavio_up_to_date = (
-    packaging.version.parse(flavio.__version__)
-    >= packaging.version.parse(__flavio__version__)
+    version.parse(flavio.__version__)
+    >= version.parse(__flavio__version__)
 )
 from . import classes
 from . import util
